e7ef942cea05b797085c0d66ff42b74e107fa1f0
Yohan de Rose <yohanderose@gmail.com>
2021-04-16 23:49:17 +1200

    fix(server): Installing kubectl from apt instead of snap

--- setup-server.sh
+++ setup-server.sh
@@ -1,5 +1,10 @@
 # Install Kubectl
-sudo apt update && sudo apt install -y snap && sudo snap install kubectl --classicgit clone https://github.com/yohanderose/object-detection-webservice.git
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
 # Install Kind
 curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.10.0/kind-linux-amd64
 chmod +x ./kind
